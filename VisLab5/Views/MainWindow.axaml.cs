6fc3946093f821b6961cbc3e07a9cdde0955a6fa
kopan <kopanov_03@mail.ru>
2023-04-23 23:37:22 +0700

    Добавьте файлы проекта.

---
+++ VisLab5/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace VisLab5.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
