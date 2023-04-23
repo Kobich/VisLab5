c00fcf341c57261e0665006a70c8092774a2e21e
kopan <kopanov_03@mail.ru>
2023-04-24 00:05:17 +0700

    lol

--- DiagramEditor/Program.cs
+++ DiagramEditor/Program.cs
@@ -1,12 +1,9 @@
-using Avalonia;
-using Avalonia.Controls.ApplicationLifetimes;
+﻿using Avalonia;
 using Avalonia.ReactiveUI;
 using System;
 
-namespace VisLab5
-{
-    internal class Program
-    {
+namespace DiagramEditor {
+    internal class Program {
         // Initialization code. Don't use any Avalonia, third-party APIs or any
         // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
         // yet and stuff might break.
